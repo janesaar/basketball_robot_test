132d106783a4fcc77961a7a9da7fef23455f3f1e
Jane Saar <jane.saar@hotmail.com>
2018-02-14 16:54:34 +0200

    Add third thrower speed input

--- public/main.js
+++ public/main.js
@@ -1,13 +1,16 @@
 var throwerSpeedInput = document.querySelector('#thrower-speed');
 var sendThrowerSpeedButton = document.querySelector('#send-thrower-speed');
 var throwerSpeedInput2 = document.querySelector('#thrower-speed2');
+var throwerSpeedInput3 = document.querySelector('#thrower-speed3');
 var sendThrowerSpeedButton2 = document.querySelector('#send-thrower-speed2');
+var sendThrowerSpeedButton3 = document.querySelector('#send-thrower-speed3');
 var stopThrowerButton = document.querySelector('#stop-thrower');
 var failsafeCheckbox = document.querySelector('#failsafe');
 
 var params = {
     throwerSpeed1: '0',
-    throwerSpeed2: '0'
+    throwerSpeed2: '0',
+    throwerSpeed3: '0'
 };
 
 throwerSpeedInput.addEventListener('keyup', function () {
@@ -20,12 +23,21 @@ throwerSpeedInput2.addEventListener('keyup', function () {
     saveParams();
 });
 
+throwerSpeedInput3.addEventListener('keyup', function () {
+    params.throwerSpeed3 = throwerSpeedInput3.value;
+    saveParams();
+});
+
 sendThrowerSpeedButton.addEventListener('click', function () {
     sendCommand('speeds:0:0:0:0:' + throwerSpeedInput.value);
 });
 
 sendThrowerSpeedButton2.addEventListener('click', function () {
     sendCommand('speeds:0:0:0:0:' + throwerSpeedInput2.value);
+});
+
+sendThrowerSpeedButton3.addEventListener('click', function () {
+    sendCommand('speeds:0:0:0:0:' + throwerSpeedInput3.value);
 });
 
 failsafeCheckbox.addEventListener('click', function () {
